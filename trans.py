ce161e6d6b30ca4957e0deec2afc425e17b7edbc
Chelozzi <forcsgowithfriends@mail.ru>
2023-04-19 14:29:15 +0500

    new

--- trans.py
+++ trans.py
@@ -1,53 +1,15 @@
-import whisper
-import librosa
-import pandas as pd
-import numpy as np
+import whisper, json
 
-# Загрузка модели Whisper
 model = whisper.load_model("base")
 
-# Загрузка аудио-файла
-audio = whisper.load_audio("C://kjk//audio.mp3")
-audio = whisper.pad_or_trim(audio)
+result = model.transcribe("audio.mp3", fp16 = False)
 
-# Вычисление спектрограммы мел
-mel = whisper.log_mel_spectrogram(audio).to(model.device)
+sentences = [segment['text'] for segment in result['segments']]
+starts = [segment['start'] for segment in result['segments']]
+sentences_starts = dict(zip(sentences, starts))
 
-# Транскрибация аудио-файла
-options = whisper.DecodingOptions(fp16 = False)
-result = whisper.decode(model, mel, options)
-text = result.text
-
-# Разделение текста на предложения
-sentences = []
-current_sentence = ''
-for i in range(len(text)):
-    if text[i].isupper() and i > 0 and text[i-1] == ' ':
-        sentences.append(current_sentence.strip())
-        current_sentence = text[i]
-    else:
-        current_sentence += text[i]
-sentences.append(current_sentence.strip())
-print(sentences)
+with open('sentences_starts.json', 'w', encoding='utf-8') as f:
+    json.dump(sentences_starts, f, ensure_ascii=False, indent=4)
+    f.close()
 
 
-# Определение временных меток для каждого предложения
-filename = "C://kjk//audio.mp3"
-audio, sr = librosa.load(filename, sr=None)
-audio = whisper.pad_or_trim(audio)
-timestamps = []
-for sentence in sentences:
-    start = result.text.index(sentence)
-    duration = librosa.get_duration(y=audio, sr=sr)
-    end = start + len(sentence)
-    end_time = librosa.frames_to_time(end * 5, sr=sr)
-    start_time = librosa.frames_to_time(start * 5, sr=sr)
-    timestamps.append((start_time, end_time))
-
-# Сохранение временных меток в таблицу
-df = pd.DataFrame(timestamps, columns=["start_time", "end_time"])
-df["sentence"] = sentences
-#df.to_csv("C://kjk//timestamps.csv", index=False)
-
-# Вывод результата
-print(df)
