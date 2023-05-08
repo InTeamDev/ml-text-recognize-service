4648ea2949ed5356d189c0e4f727838e33c34575
Chelozzi <129832578+Chelozzi@users.noreply.github.com>
2023-05-09 03:27:21 +0500

    Update trans.py

--- trans.py
+++ trans.py
@@ -3,13 +3,10 @@ import whisper, json
 model = whisper.load_model("base")
 
 result = model.transcribe("audio.mp3", fp16 = False)
-
-sentences = [segment['text'] for segment in result['segments']]
-starts = [segment['start'] for segment in result['segments']]
-sentences_starts = dict(zip(sentences, starts))
+text = result['text']
 
 with open('sentences_starts.json', 'w', encoding='utf-8') as f:
-    json.dump(sentences_starts, f, ensure_ascii=False, indent=4)
+    json.dump(text, f, ensure_ascii=False, indent=4)
     f.close()
 
 
