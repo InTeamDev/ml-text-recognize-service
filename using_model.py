a32c90f34606827afd037b367821631cd703bd88
Chelozzi <129832578+Chelozzi@users.noreply.github.com>
2023-05-09 03:37:19 +0500

    Update using_model.py

--- using_model.py
+++ using_model.py
@@ -1,15 +1,27 @@
 from itertools import groupby
 import torch
 from transformers import T5ForConditionalGeneration, T5Tokenizer
-model_name = "0x7194633/keyt5-large"
 
+MODEL_NAME = "0x7194633/keyt5-large"
 
+# Загрузка модели и токенизатора
+tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
+model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
 
-def generate(text, **kwargs):
-    inputs = tokenizer(text, return_tensors='pt')
-    with torch.no_grad():
-        hypotheses = model.generate(**inputs, num_beams=5, **kwargs)
-    s = tokenizer.decode(hypotheses[0], skip_special_tokens=True)
+def process_text(s):
+    """Функция для предобработки сгенерированного текста."""
     s = s.replace('; ', ';').replace(' ;', ';').lower().split(';')[:-1]
     s = [el for el, _ in groupby(s)]
     return s
+
+def generate(text, **kwargs):
+    """Функция для генерации текста с помощью модели."""
+    inputs = tokenizer(text, return_tensors='pt')
+    with torch.no_grad():
+        hypotheses = model.generate(**inputs, num_beams=5, **kwargs)
+    return process_text(tokenizer.decode(hypotheses[0], skip_special_tokens=True))
+
+
+article = """Жизнь человека неразрывно связана с родной природой, с землей. Земля - это наша кормилица..."""
+
+print(generate(article, top_p=1.0, max_length=64))
