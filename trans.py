fdbb51411747ce208841fc22d0ea1848b68b114f
Chelozzi <129832578+Chelozzi@users.noreply.github.com>
2023-05-09 03:30:56 +0500

    Update trans.py

--- trans.py
+++ trans.py
@@ -1,12 +1,12 @@
-import whisper, json
+import whisper
 
 model = whisper.load_model("base")
 
 result = model.transcribe("audio.mp3", fp16 = False)
 text = result['text']
 
-with open('sentences_starts.json', 'w', encoding='utf-8') as f:
-    json.dump(text, f, ensure_ascii=False, indent=4)
-    f.close()
+with open('text.txt', 'w', encoding='utf-8') as f:
+    f.write(text)
+
 
 
