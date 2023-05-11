8f395b6f783acbf88669757423b6a7c7b459f1ce
Chelozzi <129832578+Chelozzi@users.noreply.github.com>
2023-05-11 13:30:38 +0500

    Update trans.py

--- src/ml/trans.py
+++ src/ml/trans.py
@@ -5,5 +5,5 @@ model = whisper.load_model("base")
 result = model.transcribe("audio.mp3", fp16=False)
 text = result['text']
 
-with open('text.txt', 'w', encoding='utf-8') as f:
+with open('learning_data/text.txt', 'w', encoding='utf-8') as f:
     f.write(text)
