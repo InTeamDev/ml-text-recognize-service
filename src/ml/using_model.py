7a0c8a6bfa7a7a9ef8d79ae12d16c7d5e33cbe6e
Pavel <tpptgs@bk.ru>
2023-05-17 16:29:29 +0500

    Revert "Update using_model.py"
    This reverts commit d9381a4954fa5ea867b5d8d048850d7655f5ebc7.

--- src/ml/using_model.py
+++ src/ml/using_model.py
@@ -1,5 +1,4 @@
 from itertools import groupby
-
 import torch
 from transformers import T5ForConditionalGeneration, T5Tokenizer
 
@@ -9,15 +8,14 @@ MODEL_NAME = "keyt5-craft"
 tokenizer = T5Tokenizer.from_pretrained(MODEL_NAME)
 model = T5ForConditionalGeneration.from_pretrained(MODEL_NAME)
 
-
+# Предобработки сгенерированного текста
 def process_text(s):
     s = s.replace('; ', ';').replace(' ;', ';').lower().split(';')[:-1]
     s = [el for el, _ in groupby(s)]
     return s
 
-
+# Функция для генерации текста с помощью модели
 def generate(text, **kwargs):
-    """Функция для генерации тэгов."""
     inputs = tokenizer(text, return_tensors='pt')
     with torch.no_grad():
         hypotheses = model.generate(**inputs, num_beams=5, **kwargs)
@@ -25,11 +23,11 @@ def generate(text, **kwargs):
 
 
 # Чтение текста из файла
-with open("learning_data/text.txt", "r") as file:
-    text = file.read()
+with open("text.txt", "r") as file:
+    article = file.read()
 
-tags = set(generate(text, top_p=1.0, max_length=64))
+tags = set(generate(article, top_p=1.0, max_length=64))
 
 # Запись результата в файл
-with open("learning_data/text_tags.txt", "w") as file:
-        file.write(f"{text};{tags}")
+with open("output.txt", "w") as file:
+        file.write(f"{article};{tags}")
