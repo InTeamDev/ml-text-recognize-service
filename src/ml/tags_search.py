79b824a589df5c510009797c970e8f39b90db19c
Pavel <36643494+ZetoOfficial@users.noreply.github.com>
2023-05-28 16:12:16 +0500

    Update tags (#4)
    * fix conflicts
    
    * update tags service
    
    ---------
    
    Co-authored-by: Pavel Titov <zeto@MacBook-Pro-Pavel.local>
    Co-authored-by: Chelozzi <forcsgowithfriends@mail.ru>

--- src/ml/tags_search.py
+++ src/ml/tags_search.py
@@ -1,17 +1,17 @@
 import pymorphy2
 import yake
-
+from string import punctuation
 
 class KeywordService:
     def __init__(
         self,
         text,
         language="ru",
-        max_ngram_size=2,
+        max_ngram_size=1,
         deduplication_threshold=0.9,
         deduplication_algo='seqm',
         windowSize=1,
-        numOfKeywords=20,
+        numOfKeywords=10000,
     ):
         self.text = text
         self.language = language
@@ -34,17 +34,50 @@ class KeywordService:
             features=None,
         )
         keywords = custom_kw_extractor.extract_keywords(self.text)
-        keywords = [keyword[0] for keyword in keywords]
         return keywords
 
+    def lemmatize_text(self):
+        words = self.text.split()
+        lemmatized_words = list()
+        for word in words:
+            word_save = word
+            punct = ""
+            if (word[-1] in punctuation):
+                punct = word[-1]
+                word = word[:-1]
+                word_save = word
+            word = (self.morph.parse(word)[0].inflect({'sing'}))
+            if (word == None):
+                word = word_save
+            else:
+                word = word.normal_form
+            word += punct
+            lemmatized_words.append(word)
+        lemmatized_text = ' '.join(lemmatized_words)
+        return lemmatized_text
+    
     def normalize_keywords(self, input_keywords):
-        keywords = set()
+        keywords = list()
         for keyword in input_keywords:
-            if self.morph.parse(keyword)[0].tag.POS in self.tokens:  # type: ignore
-                keywords.add(self.morph.parse(keyword)[0].normal_form if len(keyword.split()) == 1 else keyword)
+            if self.morph.parse(keyword[0])[0].tag.POS in self.tokens:  # type: ignore
+                if (keyword[0][-1] in punctuation):
+                    keyword = (keyword[0][:-1], keyword[1])
+                keywords.append(keyword)          
         return keywords
-
+    
+    def sort_keywords_frequency(self, keywords):
+        keywords.sort(key = lambda keyword: keyword[1])
+        keywords = list(reversed(keywords))
+        return keywords
+    
+    def return_first_max_frequency(self, keywords, count):
+        count_keywords = 20
+        return list(map(lambda keyword: keyword[0], keywords[:count_keywords]))
+        
     def generate_tags(self):
+        self.text = self.lemmatize_text()
         keywords = self.extract_keywords()
         keywords = self.normalize_keywords(keywords)
+        keywords = self.sort_keywords_frequency(keywords)
+        keywords = self.return_first_max_frequency(keywords, 20)
         return keywords
